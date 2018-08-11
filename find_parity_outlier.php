ee7d73b139f3e7f22ac17b8e8c7e58ade1b90dff
Divya <divyasharath86@gmail.com>
2018-08-11 22:43:08 +0800

    parityoutliercode

---
+++ find_parity_outlier.php
@@ -0,0 +1,15 @@
+<?php
+
+function find($integers) {
+
+	$array_count_odd = array_count_values(array_filter($integers, "odd"));
+	$array_count_even = array_count_values(array_filter($integers, "even"));
+
+	if(count($array_count_even) == 1)
+		return  key($array_count_even);
+	else if(count($array_count_odd) == 1)
+		return  key($array_count_odd);
+	 
+}
+echo find([100, 101, 102]);
+?>
