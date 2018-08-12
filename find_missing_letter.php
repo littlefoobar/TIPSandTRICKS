f2a29068ff2d3c57303ed390c4bed8d865af9c12
littlefoobar <42287093+littlefoobar@users.noreply.github.com>
2018-08-12 20:48:14 +0800

    Deleting code
    will fetch from master !

--- find_missing_letter.php
+++ find_missing_letter.php
@@ -1,9 +1,4 @@
 <?php
-function find_missing_letter(array $array): string {
 
-	return implode(array_diff(range(current($array),end($array)),$array));
-	
-}
-echo find_missing_letter(["O", "Q", "R", "S"]);
 
 ?>
